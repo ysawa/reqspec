7dff2de1d27364b36d0dea0fb02ac66cf68ae90d
Yoshihiro Sawa <yoshihirosawa@gmail.com>
2013-04-04 21:09:36 +0900

    add: test for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be initialized' do
+    user = User.new
+    user.should_not be_persisted
+  end
 end
